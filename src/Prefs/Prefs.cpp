406b19b38221c4290129df23ff81f6f584d238b2
Caleb Theil <86980706+ctheil@users.noreply.github.com>
2026-05-31 15:41:06 -0600

    feat: wip prefs and flash

--- src/Prefs/Prefs.cpp
+++ src/Prefs/Prefs.cpp
@@ -5,15 +5,16 @@
 
 Preferences flashPreferences;
 
-float Prefs::publishFrequencyMinutes = 15.0;
+float Prefs::publishFrequency = 15.0;
 std::string Prefs::deviceId = "untitled_dev_board"; // default
 
-char *const DEVICE_ID_KEY = "device-id";
-
+void Prefs::syncWithFlash()
+{
+}
 void Prefs::setup()
 {
   flashPreferences.begin("canopy", false);
-  std::string setId = std::string(flashPreferences.getString(DEVICE_ID_KEY).c_str());
+  std::string setId = std::string(flashPreferences.getString(pkeys.deviceId).c_str());
   if (setId != "")
     Prefs::deviceId = setId;
 
@@ -21,7 +22,7 @@ void Prefs::setup()
   Comms::addTopic(Comms::endpoint("/preferences/put-publish-freq"), [&](std::string payload)
                   { Prefs::publishFrequencyMinutes = std::stof(payload); Prefs::publishState(); });
   Comms::addTopic(Comms::endpoint("/preferences/put-device-id"), [&](std::string payload)
-                  { flashPreferences.putString(DEVICE_ID_KEY, payload.c_str()); Prefs::deviceId = payload; Prefs::publishState(); }); // WILL PUBLISH TO UPDATED DEVICE ID ENDPOINT... FIXME?
+                  { flashPreferences.putString(pkeys.deviceId, payload.c_str()); Prefs::deviceId = payload; Prefs::publishState(); }); // WILL PUBLISH TO UPDATED DEVICE ID ENDPOINT... FIXME?
 }
 
 void Prefs::publishState()
@@ -30,5 +31,5 @@ void Prefs::publishState()
       Comms::endpoint("/preferences").c_str(),
       0,
       true, // retain
-      std::string(std::to_string(Prefs::publishFrequencyMinutes) + "," + Prefs::deviceId).c_str());
+      std::string(std::to_string(Prefs::publishFrequency) + "," + Prefs::deviceId).c_str());
 }
--- src/Prefs/Prefs.h
+++ src/Prefs/Prefs.h
@@ -3,7 +3,19 @@
 #include "../Comms/Comms.h"
 #include <string>
 #include <Preferences.h>
+// struct PrefKeys
+// {
+//   char *deviceId, *publishFreq, *wifiSsid, *wifiPassword;
+// };
+struct FlashPref
+{
+  char *key, *value;
+};
 
+struct Pref
+{
+  char *flashKey, char *
+};
 class Prefs
 {
 public:
@@ -11,4 +23,17 @@ public:
   static float publishFrequencyMinutes;
   static std::string deviceId;
   static void publishState();
+
+private:
+  static void syncWithFlash();
+  inline static std::vector<FlashPref> flashPrefs = {
+      FlashPref{"device-id", "deviceId"},
+      FlashPref{"publish-frequency", "publish-frequency"},
+  };
+  // inline static PrefKeys pkeys{
+  //   deviceId : "device-id",
+  //   publishFreq : "publish-frequency",
+  //   wifiSsid : "wifi-ssid",
+  //   wifiPassword : "wifi-password"
+  // };
 };
