3ebb4eb64a08e46ba3519e15876751f16ce51f19
Caleb Theil <86980706+ctheil@users.noreply.github.com>
2026-05-31 17:07:51 -0600

    chore: bump version

--- src/main.cpp
+++ src/main.cpp
@@ -25,7 +25,7 @@ struct ProgramInfo
 {
   std::string version;
   int isDevelopment;
-} pinfo = ProgramInfo{"v0.0.04", 0};
+} pinfo = ProgramInfo{"v0.0.05", 0};
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /// FUNCTIONS
